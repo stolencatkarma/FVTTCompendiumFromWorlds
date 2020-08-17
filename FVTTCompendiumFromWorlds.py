384dd55b83ea1607307b1be2dcf3449fdfc49b64
stolencatkarma <stolencatkarma@users.noreply.github.com>
2020-08-17 18:36:45 -0400

    Update FVTTCompendiumFromWorlds.py

--- FVTTCompendiumFromWorlds.py
+++ FVTTCompendiumFromWorlds.py
@@ -1,5 +1,5 @@
 # this creates a module from worlds and iterates it into a compendium.
-# put this in /Data/ directory and run it with python3 ripData.py
+# put this in /Data/ directory and run it with python3 FVTTCompendiumFromWorlds.py
 
 # go through data/worlds and extract the databases and files to generate a compendium mod
 import os
